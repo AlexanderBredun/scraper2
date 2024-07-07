0f96b20dd54ae65e1a56f4881c9f8c784229bba0
Alexandr Bredun <alexandr@katsubet.com>
2024-07-07 11:57:18 +0300

    sad

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -6,6 +6,7 @@ const scrapeLogic = async (res) => {
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
+      "--single-process",
       "--no-zygote",
     ],
     executablePath:
@@ -17,8 +18,23 @@ const scrapeLogic = async (res) => {
     const page = await browser.newPage();
 
     await page.goto("https://developer.chrome.com/");
-   
-    const fullTitle = await page.evaluate('document.querySelector("#span-stylecolor-var-chrome-primary-span").innerHTML');
+
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    // Type into search box
+    await page.type(".search-box__input", "automate beyond recorder");
+
+    // Wait and click on first result
+    const searchResultSelector = ".search-box__link";
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+
+    // Locate the full title with a unique string
+    const textSelector = await page.waitForSelector(
+      "text/Customize and automate"
+    );
+    const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
     // Print the full title
     const logStatement = `The title of this blog post is ${fullTitle}`;
