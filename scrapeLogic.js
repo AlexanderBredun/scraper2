c72dbb2e9e789b20b210e4e63a4a21f0f651ac1e
Alexandr Bredun <alexandr@katsubet.com>
2024-07-07 13:23:21 +0300

    asdas

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -22,17 +22,17 @@ const scrapeLogic = async (res) => {
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+    // // Type into search box
+    // await page.type(".search-box__input", "automate beyond recorder");
 
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+    // // Wait and click on first result
+    // const searchResultSelector = ".search-box__link";
+    // await page.waitForSelector(searchResultSelector);
+    // await page.click(searchResultSelector);
 
     // Locate the full title with a unique string
     const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
+      "#span-stylecolor-var-chrome-primary-span"
     );
     const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
